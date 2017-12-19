23f80c963129d7541833f32e1a92504cdc6f166e
Peter <Peter@PETER-915>
2017-12-19 21:56:52 +0800

    a

--- LibrarySystemBackEnd/ClassBackEnd.cs
+++ LibrarySystemBackEnd/ClassBackEnd.cs
@@ -12,10 +12,137 @@ namespace LibrarySystemBackEnd
 	{
 		private ClassSQL sql;
 
+		#region 私有方法
+		/// <summary>
+		/// 在数据库中精确查找用户
+		/// </summary>
+		/// <param name="userid">待查找的用户id</param>
+		/// <returns>有满足的用户返回用户实例，没有返回null</returns>
+		private ClassUserBasicInfo getUsersBasic(string userid)
+		{
+			ClassUserBasicInfo users = null;
+			using (SqlConnection con = new SqlConnection(sql.Builder.ConnectionString))
+			{
+				SqlCommand cmd = con.CreateCommand();
+				cmd.CommandText = "select *from dt_UserBasic where userId=@a";
+				cmd.Parameters.Clear();
+				cmd.Parameters.AddWithValue("@a", userid);
+
+				con.Open();
+				SqlDataReader dr = cmd.ExecuteReader();
+				if (dr.HasRows)
+				{
+					while (dr.Read())
+					{
+						users = new ClassUserBasicInfo(dr);
+					}
+				}
+			}
+			return users;
+		}
+
+		/// <summary>
+		/// 精确查找管理员
+		/// </summary>
+		/// <param name="id">管理员id</param>
+		/// <returns>管理员实例</returns>
+		private ClassAdmin getAdmin(string id)
+		{
+			ClassAdmin admin = null;
+			using (SqlConnection con = new SqlConnection(sql.Builder.ConnectionString))
+			{
+				SqlCommand cmd = con.CreateCommand();
+				cmd.CommandText = "select *from dt_Admin where id=@a";
+				cmd.Parameters.Clear();
+				cmd.Parameters.AddWithValue("@a", id);
+
+				con.Open();
+				SqlDataReader dr = cmd.ExecuteReader();
+				if (dr.HasRows)
+				{
+					while (dr.Read())
+					{
+						admin = new ClassAdmin(dr);
+					}
+				}
+			}
+			return admin;
+		}
+
+		/// <summary>
+		/// 向数据库添加用户
+		/// </summary>
+		/// <param name="user">待添加的用户</param>
+		/// <returns>成功返回true，失败(学号已存在)返回false</returns>
+		private bool AddUser(ClassUserBasicInfo user)
+		{
+			if (getUsersBasic(user.UserId) != null)
+				return false;
+			using (SqlConnection con = new SqlConnection(sql.Builder.ConnectionString))
+			{
+				SqlCommand cmd = con.CreateCommand();
+				cmd.CommandText = "insert into dt_UserBasic values(@a,@b,@c,@d,@e,@f,@g,@h,@i,@j,@k)";
+				cmd.Parameters.Clear();
+				cmd.Parameters.AddWithValue("@a", user.UserId);
+				cmd.Parameters.AddWithValue("@b", user.UserName);
+				cmd.Parameters.AddWithValue("@c", user.UserPassword);
+				cmd.Parameters.AddWithValue("@d", user.UserSchool);
+				cmd.Parameters.AddWithValue("@e", user.UserType);
+				cmd.Parameters.AddWithValue("@f", user.UserCurrentScheduleAmount);
+				cmd.Parameters.AddWithValue("@g", user.UserMaxBorrowableAmount);
+				cmd.Parameters.AddWithValue("@h", user.UserCurrentBorrowedAmount);
+				cmd.Parameters.AddWithValue("@i", user.UserCurrentMaxBorrowableAmount);
+				cmd.Parameters.AddWithValue("@j", user.UserCredit);
+				cmd.Parameters.AddWithValue("@k", user.UserRegisterDate);
+
+				con.Open();
+				int count = cmd.ExecuteNonQuery();
+				if (count > 0)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		/// <summary>
+		/// 向数据库添加用户
+		/// </summary>
+		/// <param name="admin">待添加的管理员</param>
+		/// <returns>成功返回true，失败(学号已存在)返回false</returns>
+		private bool AddAdmin(ClassAdmin admin)
+		{
+			if (getAdmin(admin.Id) != null)
+				return false;
+			using (SqlConnection con = new SqlConnection(sql.Builder.ConnectionString))
+			{
+				SqlCommand cmd = con.CreateCommand();
+				cmd.CommandText = "insert into dt_Admin values(@a,@b,@c,@d,@e)";
+				cmd.Parameters.Clear();
+				cmd.Parameters.AddWithValue("@a", admin.Id);
+				cmd.Parameters.AddWithValue("@b", admin.Name);
+				cmd.Parameters.AddWithValue("@c", admin.Password);
+				cmd.Parameters.AddWithValue("@d", admin.Type);
+				cmd.Parameters.AddWithValue("@e", admin.RegisterDate);
+
+				con.Open();
+				int count = cmd.ExecuteNonQuery();
+				if (count > 0)
+					return true;
+				else
+					return false;
+			}
+		}
+		#endregion
+
+		#region 公有方法
+		/// <summary>
+		/// 构造，初始化SQL连接
+		/// </summary>
 		public ClassBackEnd()
 		{
 			sql = new ClassSQL();
 		}
+
 		/// <summary>
 		/// 用户登录函数，加载个人信息；管理员登录函数，加载用户信息
 		/// </summary>
@@ -24,8 +151,8 @@ namespace LibrarySystemBackEnd
 		/// <returns>管理员登录成功返回2，用户登录成功返回1,失败返回0(用户名不存在，密码不正确)</returns>
 		public int Login(string id, string password)
 		{
-			ClassUserBasicInfo user = sql.getUsersBasic(id);
-			ClassAdmin admin = sql.getAdmin(id);
+			ClassUserBasicInfo user = getUsersBasic(id);
+			ClassAdmin admin = getAdmin(id);
 			if (user == null && admin == null) return 0;
 			else if (user != null)
 			{
@@ -40,6 +167,7 @@ namespace LibrarySystemBackEnd
 			}
 			return 0;
 		}
+
 		/// <summary>
 		/// 注册用户函数，会检查id是否与现有用户重复
 		/// </summary>
@@ -51,9 +179,10 @@ namespace LibrarySystemBackEnd
 		/// <returns>返回值：0id重复，1成功</returns>
 		public bool RegisterUser(string userid, string username, string password, string school, USERTYPE usertype)
 		{
-			bool fl = sql.AddUser(new ClassUserBasicInfo(userid, username, password, school, usertype));
+			bool fl = AddUser(new ClassUserBasicInfo(userid, username, password, school, usertype));
 			return fl;
 		}
+
 		/// <summary>
 		/// 管理员注册
 		/// </summary>
@@ -64,16 +193,17 @@ namespace LibrarySystemBackEnd
 		/// <returns>返回值：0id重复，1成功</returns>
 		public bool RegisterAdmin(string id, string name, string password, USERTYPE type)
 		{
-			bool fl = sql.AddAdmin(new ClassAdmin(id, name, password, type));
+			bool fl = AddAdmin(new ClassAdmin(id, name, password, type));
 			return fl;
 		}
+
 		/// <summary>
 		/// 借书
 		/// </summary>
 		/// <param name="userid">用户id</param>
 		/// <param name="password">用户密码</param>
 		/// <param name="bookid">书籍id</param>
-		/// <returns>返回：0借书成功，1用户借书数量上限，2没有可借书籍，3用户已经借阅过，4</returns>
+		/// <returns>返回：0借书成功，1用户借书数量上限，2没有可借书籍，3其他错误</returns>
 		public int BorrowBook(string userid, string password, string bookid)
 		{
 			int res = 0;
@@ -111,13 +241,7 @@ namespace LibrarySystemBackEnd
 
 					if (cmd2.ExecuteNonQuery()<=0)
 					{
-						string sqlstr3 = @"SET ROWCOUNT 1 update dt_abook 
-									set bookState = 1,
-									borrowUserId = '" + userid +
-									   "', borrowTime = '" + DateTime.Now +
-									   @"', returnTime = '" + (DateTime.Now + ClassUserBasicInfo.DefaultDate) +
-									   "'where bookIsbn like '" + bookid +
-									   "%' and bookState = 0 SET ROWCOUNT 0";
+						string sqlstr3 = "SET ROWCOUNT 1 update dt_abook set bookState = 1,borrowUserId = '" + userid + "', borrowTime = '" + DateTime.Now + "', returnTime = '" + (DateTime.Now + "'where bookIsbn like '" + bookid + "%' and bookState = 0 SET ROWCOUNT 0");
 
 						SqlCommand cmd3 = new SqlCommand();
 						cmd3.Connection = con;
@@ -138,13 +262,89 @@ namespace LibrarySystemBackEnd
 				catch (Exception e)
 				{
 					if (res == 0)
-						res = 4;
+						res = 3;
 					tra.Rollback();
 				}
 			}
 			return res;
 		}
 
+		/// <summary>
+		/// 向数据库中添加书籍，采用数据库事物
+		/// </summary>
+		/// <param name="adminId"></param>
+		/// <param name="adminPassword"></param>
+		/// <param name="bk"></param>
+		/// <returns></returns>
+		public bool AddBook(string adminId,string adminPassword, ClassBook bk)
+		{
+			bool res = false;
+			using (SqlConnection con = new SqlConnection(sql.Builder.ConnectionString))
+			{
+				con.Open();
+				SqlTransaction tra = null;
+				try
+				{
+					tra = con.BeginTransaction();
+					SqlCommand cmd = new SqlCommand();
+					cmd.Transaction = tra;
+					cmd.Connection = con;
+
+					cmd.CommandType = CommandType.Text;
+					cmd.CommandText = "insert into dt_book values(@a,@b,@c,@d,@e,@f,@g,@h,@i,@j,@k)";
+					cmd.Parameters.Clear();
+					cmd.Parameters.AddWithValue("@a", bk.BookName);
+					cmd.Parameters.AddWithValue("@b", bk.BookIsbn);
+					cmd.Parameters.AddWithValue("@c", bk.BookPublisher);
+					cmd.Parameters.AddWithValue("@d", bk.BookAuthor);
+					cmd.Parameters.AddWithValue("@e", bk.BookImage);
+					cmd.Parameters.AddWithValue("@f", bk.BookIntroduction);
+					cmd.Parameters.AddWithValue("@g", bk.BookPublishTime);
+					cmd.Parameters.AddWithValue("@h", bk.BookAmount);
+					cmd.Parameters.AddWithValue("@i", bk.BookLable1);
+					cmd.Parameters.AddWithValue("@j", bk.BookLable2);
+					cmd.Parameters.AddWithValue("@k", bk.BookLable3);
+
+					if (cmd.ExecuteNonQuery() < 0)
+						throw new Exception();
+					foreach (ClassABook abk in bk.Book)
+					{
+						cmd.CommandType = CommandType.Text;
+						cmd.CommandText = "insert into dt_abook values(@a,@b,@c,@d,@e,@f,@g,@h,@i,@j,@k,@l,@m)";
+						cmd.Parameters.Clear();
+
+						cmd.Parameters.AddWithValue("@a", abk.BookName);
+						cmd.Parameters.AddWithValue("@b", abk.BookIsbn);
+						cmd.Parameters.AddWithValue("@c", abk.BookPublisher);
+						cmd.Parameters.AddWithValue("@d", abk.BookAuthor);
+						cmd.Parameters.AddWithValue("@e", abk.BookImage);
+						cmd.Parameters.AddWithValue("@f", abk.BookPublishDate);
+						cmd.Parameters.AddWithValue("@g", abk.BookBroughtTime);
+						cmd.Parameters.AddWithValue("@h", abk.BookState);
+						cmd.Parameters.AddWithValue("@i", abk.BorrowUserId);
+						cmd.Parameters.AddWithValue("@j", abk.BorrowTime);
+						cmd.Parameters.AddWithValue("@k", abk.ReturnTime);
+						cmd.Parameters.AddWithValue("@l", abk.Delayed);
+						cmd.Parameters.AddWithValue("@m", abk.Deleted);
+
+						if (cmd.ExecuteNonQuery() < 0)
+							throw new Exception();
+					}
+					tra.Commit();
+					res = true;
+				}
+				catch (Exception e)
+				{
+					res = false;
+					tra.Rollback();
+				}
+			}
+			return res;
+		}
+
+
+
+		#endregion
 
 	}
 }
