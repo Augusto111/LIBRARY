18b376f6f4865862307ad0659916fc3dcd6f6ba2
Peter <Peter@PETER-915>
2017-12-25 19:40:21 +0800

    aa

---
+++ LibrarySystemBackEnd/FileProtocol.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWorkApp
+{
+	public enum FileRequestMode
+	{
+		UserLogin=0,
+		UserRegist=1,
+		SearchBook=2,
+		BookLoad=3,
+
+	}
+	class FileProtocol
+	{
+		private readonly FileRequestMode mode;
+		private readonly int port;
+		private readonly string fileName;
+
+		public FileProtocol(FileRequestMode mode, int port, string fileName)
+		{
+			this.mode = mode;
+			this.port = port;
+			this.fileName = fileName;
+		}
+
+		public FileRequestMode Mode
+		{
+			get { return mode; }
+		}
+
+		public int Port
+		{
+			get { return port; }
+		}
+
+		public string FileName
+		{
+			get { return fileName; }
+		}
+
+		public override string ToString()
+		{
+			return String.Format("<protocol><file name=\"{0}\" mode=\"{1}\" port=\"{2}\" /></protocol>"
+				, fileName, mode, port);
+		}
+	}
+}
